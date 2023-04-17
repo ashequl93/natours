3b5923de9958d8103ba1b4b4e6dd1c4f507763a1
Ashequl Haque <ashequlhaque009@gmail.com>
2023-04-17 15:45:53 -0400

    Natours initial repo setup

---
+++ index.ts
@@ -0,0 +1,10 @@
+const { add } = require('./util');
+
+const fruits = ['apple', 'banana', 'orange'];
+
+fruits.push('pineapple');
+fruits.push('dragon fruit');
+
+console.log(add(3, 5));
+
+add(7, 'f');
